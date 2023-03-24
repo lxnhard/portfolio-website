eccd5b8d08a203ffa9b973247c26efeb53cf6491
lxnhard <106260289+lxnhard@users.noreply.github.com>
2023-03-24 22:48:51 +0100

    add event listeners to ThreeJS model + swap model

--- src/cube.js
+++ src/cube.js
@@ -6,15 +6,26 @@ import {
   AnimationMixer,
   WebGLRenderer,
   sRGBEncoding,
-  MathUtils
+  MathUtils,
+  Vector2,
+  Raycaster
 } from 'three';
 
-let camera, scene, renderer;
-let mesh, mixer;
+let camera, scene, renderer, mixer;
+let mesh;
+let cubeActive;
 
 const radius = 3.2;
 let theta = 0;
 let prevTime = Date.now();
+
+const raycaster = new Raycaster();
+const mouse = new Vector2(1, 1);
+
+const container = document.getElementById('three-container');
+
+let containerWidth = document.getElementById('three-container').clientWidth;
+let containerHeight = document.getElementById('three-container').clientHeight;
 
 init();
 animate();
@@ -23,24 +34,20 @@ render();
 
 function init() {
 
-  const container = document.getElementById('three-container');
-  const containerWidth = document.getElementById('three-container').clientWidth;
-  const containerHeight = document.getElementById('three-container').clientHeight;
-
-
   camera = new PerspectiveCamera(45, containerWidth / containerHeight, 0.25, 5);
-  camera.position.set(0, 2.2, 0);
+  camera.position.set(0, 2, 0);
+  camera.lookAt(0, 0, 0);
 
   scene = new Scene();
-  scene.fog = new Fog(0xffffff, 3, 4);
+  scene.fog = new Fog(0xffffff, 1, 3);
 
   const loader = new GLTFLoader();
 
-  loader.load('./cubes_wire.glb', gltf => {
+  loader.load('./cubes_still.glb', gltf => {
     mesh = gltf.scene;
     mesh.position.set(0, 0, 0);
-
     scene.add(mesh);
+    console.log(mesh);
     mixer = new AnimationMixer(mesh);
     render();
   });
@@ -52,12 +59,12 @@ function init() {
   container.appendChild(renderer.domElement);
 
   window.addEventListener('resize', onWindowResize);
-
+  container.addEventListener('mousemove', onMouseMove);
 }
 
 function onWindowResize() {
-  const containerWidth = document.getElementById('three-container').clientWidth;
-  const containerHeight = document.getElementById('three-container').clientHeight;
+  containerWidth = document.getElementById('three-container').clientWidth;
+  containerHeight = document.getElementById('three-container').clientHeight;
 
   camera.aspect = containerWidth / containerHeight;
   camera.updateProjectionMatrix();
@@ -68,9 +75,37 @@ function onWindowResize() {
 
 }
 
+function onMouseMove(event) {
+
+  event.preventDefault();
+
+  const rect = container.getBoundingClientRect();
+  // console.log(rect);
+
+  mouse.x = ((event.clientX - rect.x) / containerWidth) * 2 - 1;
+  mouse.y = - ((event.clientY - rect.y) / containerHeight) * 2 + 1;
+  // console.log(mouse.x + ", " + mouse.y)
+}
+
 function animate() {
 
   requestAnimationFrame(animate);
+
+  if (mesh) {
+    raycaster.setFromCamera(mouse, camera);
+
+    const intersection = raycaster.intersectObject(mesh);
+
+    if (intersection.length > 0) {
+
+      console.log(intersection[0]);
+      cubeActive = intersection[0].object;
+    }
+
+    if (cubeActive) {
+      cubeActive.rotation.x += 0.01;
+    }
+  }
   render();
 
 }
@@ -79,11 +114,11 @@ function animate() {
 
 function render() {
 
-  theta += 0.3;
+  // theta += 0.1;
 
-  camera.position.x = radius * Math.sin(MathUtils.degToRad(theta));
-  camera.position.z = radius * Math.cos(MathUtils.degToRad(theta));
-  camera.lookAt(0, -.1, 0);
+  // camera.position.x = radius * Math.sin(MathUtils.degToRad(theta));
+  // camera.position.z = radius * Math.cos(MathUtils.degToRad(theta));
+  // camera.lookAt(0, -.1, 0);
 
 
   if (mixer) {
