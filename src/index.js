30e21f4dcb48f71ad2937a5fba3eaf9e830e09b1
lxnhard <106260289+lxnhard@users.noreply.github.com>
2023-04-12 17:36:40 +0200

    contact form modal => section

--- src/index.js
+++ src/index.js
@@ -1,30 +1,11 @@
 import lozad from 'lozad';
 import anime from 'animejs/lib/anime.es.js';
 import './style.css';
-// import './cube.js';
 import './form.js';
 
 
 const observer = lozad(); // lazy loads elements with default selector as '.lozad'
 observer.observe();
-
-const modal = document.querySelector(".modal");
-const trigger = document.querySelector(".contact__button");
-const closeButton = document.querySelector(".close-button");
-
-function toggleModal() {
-  modal.classList.toggle("show-modal");
-}
-
-function windowOnClick(event) {
-  if (event.target === modal) {
-    toggleModal();
-  }
-}
-
-trigger.addEventListener("click", toggleModal);
-closeButton.addEventListener("click", toggleModal);
-window.addEventListener("click", windowOnClick);
 
 
 // navigation 
@@ -51,8 +32,13 @@ const sectionProfile = document.querySelector(".section-profile");
 const sectionProfileContent = document.querySelector(".profile");
 const sectionWork = document.querySelector(".section-projects");
 const sectionWorkContent = document.querySelector(".project-list");
+const sectionContact = document.querySelector(".section-contact");
+const sectionContactContent = document.querySelector(".contact");
 const navigationLinkAbout = document.querySelector("#navigation-link-about");
 const navigationLinkWork = document.querySelector("#navigation-link-work");
+const navigationLinkContact = document.querySelector("#navigation-link-contact");
+
+let sectionDeactivate = sectionProfile;
 
 let animationInProgress = false;
 
@@ -64,11 +50,40 @@ function toggleSections(sectionActivate) {
   if (sectionActivate.classList.contains("section--hidden")) {
 
     // identify DOM elements
-    const sectionDeactivate = (sectionActivate == sectionProfile) ? sectionWork : sectionProfile;
-    const headerActivate = (sectionActivate == sectionProfile) ? '.section-profile .section__header .letter' : '.section-projects .section__header .letter'
-    const headerDeactivate = (sectionActivate != sectionProfile) ? '.section-profile .section__header .letter' : '.section-projects .section__header .letter'
-    const contentActivate = (sectionActivate == sectionProfile) ? sectionProfileContent : sectionWorkContent;
-    const contentDeactivate = (sectionActivate != sectionProfile) ? sectionProfileContent : sectionWorkContent;
+    // const sectionDeactivate;
+    let headerActivate;
+    let contentActivate;
+    let headerDeactivate;
+    let contentDeactivate;
+
+    switch (sectionActivate) {
+      case sectionProfile:
+        headerActivate = '.section-profile .section__header .letter';
+        contentActivate = sectionProfileContent;
+        break
+      case (sectionWork):
+        headerActivate = '.section-projects .section__header .letter';
+        contentActivate = sectionWorkContent;
+        break
+      case (sectionContact):
+        headerActivate = '.section-contact .section__header .letter';
+        contentActivate = sectionContactContent;
+    }
+
+    switch (sectionDeactivate) {
+      case sectionProfile:
+        headerDeactivate = '.section-profile .section__header .letter'
+        contentDeactivate = sectionProfileContent;
+        break
+      case (sectionWork):
+        headerDeactivate = '.section-projects .section__header .letter'
+        contentDeactivate = sectionWorkContent;
+        break
+      case (sectionContact):
+        headerDeactivate = '.section-contact .section__header .letter'
+        contentDeactivate = sectionContactContent;
+    }
+
 
     // animate
     anime.timeline({ loop: false })
@@ -103,20 +118,24 @@ function toggleSections(sectionActivate) {
         easing: 'easeInSine',
         complete: function () {
           animationInProgress = false;
+          sectionDeactivate = sectionActivate;
         }
       }, '-=520')
   }
   animationInProgress = false;
+
 }
 
 navigationLinkAbout.addEventListener("click", () => { toggleSections(sectionProfile) });
 navigationLinkWork.addEventListener("click", () => { toggleSections(sectionWork) });
+navigationLinkContact.addEventListener("click", () => { toggleSections(sectionContact) });
 
 
 // H2 animation
 let textWrapper = document.querySelectorAll('.section__header .letters');
 textWrapper[0].innerHTML = textWrapper[0].textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 textWrapper[1].innerHTML = textWrapper[1].textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+textWrapper[2].innerHTML = textWrapper[2].textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
 anime.timeline({ loop: false })
   .add({
